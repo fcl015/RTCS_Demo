0c62f6c2d35f1f6686a25091393b36b6fb3c9ce3
Unknown <daniel-terrazas@hotmail.com>
2017-09-14 21:22:30 -0600

    LabPractice Done

--- io_devices.h
+++ io_devices.h
@@ -8,6 +8,12 @@
 #ifndef IO_DEVICES_H_
 #define IO_DEVICES_H_
 
-
+void ADC1_init(void);
+void PWM_init(void);
+void Serial_Init(void);
+int Serial_Send(unsigned char data);
+void Serial_Send_Frame(unsigned char *ch, unsigned char len);
 
 #endif /* IO_DEVICES_H_ */
+
+
