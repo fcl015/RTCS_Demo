f6d37191b26a406b3c5f53a4feb37c83ea3371f4
Unknown <luisa.tapiad@gmail.com>
2017-10-17 17:00:33 -0600

    Define functions

--- io_devices.c
+++ io_devices.c
@@ -4,5 +4,10 @@
  *  Created on: Oct 17, 2017
  *      Author: Luisa
  */
+void ADC_int(void);
+void PWM_init(void);
+void Serial_Init(void);
+void Serial_Send(void);
+void Serial_Send_Frame(void);
 
 
