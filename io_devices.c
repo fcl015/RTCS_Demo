2974132c095bb5c7a19a1f579de001e9b64df47b
Unknown <ecaraveo94@gmail.com>
2017-10-16 19:09:54 -0600

    Create Files

---
+++ io_devices.c
@@ -0,0 +1,8 @@
+/*
+ * io_devices.c
+ *
+ *  Created on: 16/10/2017
+ *      Author: casa
+ */
+
+
---
+++ io_devices.h
@@ -0,0 +1,13 @@
+/*
+ * io_devices.h
+ *
+ *  Created on: 16/10/2017
+ *      Author: casa
+ */
+
+#ifndef IO_DEVICES_H_
+#define IO_DEVICES_H_
+
+
+
+#endif /* IO_DEVICES_H_ */
